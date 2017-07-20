a4620f029ba00bd2ac49420b48bda5318f9279e7
Jordan Hudgens <jordan@devcamp.com>
2017-07-20 15:50:36 +0000

    Implemented intial connection to Twitter API

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,12 +59,14 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-  def alerts
-    alert= (flash[:alert]|flash[:error]||flash[:notice])
-    if alert
-      alert_generator alert
-    end
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+  if alert
+    alert_generator alert
   end
+end
+
   
   def alert_generator msg
     js add_gritter(msg, title: "Juyoung Kim Portfolio", sticky: false)
